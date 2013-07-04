a24ac4935090275da58587c994829dd605db7b1e
Graham Pentheny <graham.pentheny@gmail.com>
2013-07-04 01:38:05 -0400

    Restored test header, changed some for's to foreach's.

--- internal/elispImpl.h
+++ internal/elispImpl.h
@@ -59,20 +59,16 @@ namespace elisp {
 		Environment* newEnv = new Environment(env);
 
 		// Match the arguments to the parameters.
-		vector<symbol_cell*>::const_iterator parameterIter = mParameters.begin();
-		vector<symbol_cell*>::const_iterator parametersEnd = mParameters.end();
-		for (; parameterIter != parametersEnd; ++parameterIter) {
+		for (auto paramID : mParameters) {
 			trueOrDie(args != empty_list, "insufficient arguments provided to function");
-			newEnv->mSymbolMap[(*parameterIter)->identifier] = currentEnv.eval(args->car);
+			newEnv->mSymbolMap[paramID->identifier] = currentEnv.eval(args->car);
 			args = args->cdr;
 		}
 
 		// Evaluate the body expressions with the new environment.  Return the result of the last body expression.
 		cell_t* returnVal;
-		vector<cell_t*>::iterator bodyExprIter = mBodyExpressions.begin();
-		vector<cell_t*>::iterator bodyExprsEnd = mBodyExpressions.end();
-		for (; bodyExprIter != bodyExprsEnd; ++bodyExprIter)
-			returnVal = newEnv->eval(*bodyExprIter);
+		for (auto bodyExpr : mBodyExpressions)
+			returnVal = newEnv->eval(bodyExpr);
 
 		return returnVal;
 	}
