8760e354fe8cc77dfafacb5adf5ba3f2a07d6356
Graham Pentheny <mumblingdutch@gmail.com>
2013-07-05 16:58:28 -0400

    changed test.scm a bit and added README

--- test.scm
+++ test.scm
@@ -7,13 +7,9 @@
 (display (inc 4))
 
 ; returns the nth fibbonacci number
-(define (fib n)
-  (if (= n 1) 1
-	(if (= n 2) 1
-	  (+ (fib (- n 1)) (fib (- n 2))))))
+(define (fib n) (if (= n 1) 1 (if (= n 2) 1 (+ (fib (- n 1)) (fib (- n 2)))))) 
 
-(display (fib 10))
+(display "this should be 55: " (fib 10))
 
 (define (x f) (lambda (g) (+ f g)))
-
-(display ((x 4) 3))
+(display "this should be 7: " ((x 4) 3))
